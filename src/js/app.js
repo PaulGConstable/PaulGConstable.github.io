46f09f494374a0ca3e2beec546b103853bdb3a07
PaulGConstable <paulgconstable@btinternet.com>
2016-03-06 13:12:48 +0000

    Fix: observable array error and add markers again

--- src/js/app.js
+++ src/js/app.js
@@ -31,29 +31,45 @@ var markers = function(data){
 	this.name = ko.observable(data.name);
     this.lat = ko.observable(data.lat);
     this.lng = ko.observable(data.lng);
+    this.marker = ko.observable();
 };
 
 var viewModel = function(){
 	var self = this;
 
-	this.mapView = ko.observableArray([]);
+	this.locationList = ko.observableArray([]);
 
-	 // Gets data from Markers function and puts in
+	 // Gets data from markers function and puts in
 	 // KO observable array above
-	favLocations.forEach(function(startLocation){
-		mapView.locationList.push( new markers(startLocation))
+	favLocations.forEach(function(cycleLocation){
+		self.locationList.push(new markers(cycleLocation));
 	});
 
 	//Info window for markers
     var infoWindow = new google.maps.InfoWindow();
 
+    // Variable for marker data
+    var pin;
+
+    //iterate over observable array
+   	this.locationList().forEach(function(cycleLocation){
+   		pin = new google.maps.Marker({
+		position: new google.maps.LatLng(cycleLocation.lat(), cycleLocation.lng()),
+		map: map,
+		title: cycleLocation.name(),
+		animation: google.maps.Animation.DROP
+		});
+
+		cycleLocation.marker = pin;
+   	});
+
+
 };
 
 // Initialize the Map
 var map = new google.maps.Map(document.getElementById('map'), {
-	center: new google.maps.LatLng(51.450210,2.180518),
+	center: new google.maps.LatLng(51.450210, 2.180518),
 	zoom: 6
 });
 
-
-ko.applyBindings(new initMap());
+ko.applyBindings(new viewModel());
